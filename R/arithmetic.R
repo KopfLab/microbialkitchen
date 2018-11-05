df483ca332ce76476b75174d8089491a498f6fe5
Sebastian Kopf <sebastian.kopf@colorado.edu>
2018-11-04 21:19:09 -0700

    update arithmetic operations

--- R/arithmetic.R
+++ R/arithmetic.R
@@ -1,21 +1,34 @@
-#' Arithmetic
-#' 
-#' These arithmetic operators for quantities in chemtools
-#' keep track of units in standard operations.
-#' 
-#' @name arithmetic
+# make sure classes are loaded before everything else
 #' @include classes.R
 NULL
 
+#' Arithmetic
+#' 
+#' These arithmetic operators for quantities in chemtools keep track of units in standard operations (i.e. they ALL take the SI prefix into consideration). Also note that all operations that result in a new quantity object automatically scale the new value using \code{\link{best_metric}}.
+#' 
+#' @name arithmetic
+NULL
+
+
+
 # helper functions that informs about operation error
 operation_error <- function(operation, e1, e2) {
-  stop(sprintf("%s is not implemented for these quantities (trying to use '%s' and '%s'). ", operation, class(e1), class(e2)))
+  stop(sprintf("%s is not implemented for these quantities (trying to use '%s' and '%s'). ", operation, class(e1), class(e2)), call. = FALSE)
 }
 class_check <- function(operation, e1, e2) {
   if (class(e1) != class(e2)) operation_error(operation, e1, e2)
 }
 
 # Comparisons =======================
+
+#' @usage qty == qty, qty != qty, qty < qty, qty <= qty, qty > qty, qty >= qty
+#' @details
+#' \code{qty ==, !=, <, <=, >, >= qty} allows the comparison of quantities that are the same type (e.g. all mass).
+#' @examples 
+#' qty(1, "g") == qty(1000, "mg") # TRUE
+#' qty(2, "mg") < qty(5, "ng")  # FALSE
+#' @name arithmetic 
+NULL
 
 setMethod("<", signature(e1 = "Quantity", e2 = "Quantity"), function(e1, e2) {
   class_check ("comparison", e1, e2)
@@ -37,8 +50,26 @@ setMethod(">=", signature(e1 = "Quantity", e2 = "Quantity"), function(e1, e2) {
   return (scale_metric(e1, get_prefix(e2))@.Data >= e2@.Data)
 })
 
+setMethod("==", signature(e1 = "Quantity", e2 = "Quantity"), function(e1, e2) {
+  class_check ("comparison", e1, e2)
+  return (scale_metric(e1, get_prefix(e2))@.Data == e2@.Data)
+})
+
+setMethod("!=", signature(e1 = "Quantity", e2 = "Quantity"), function(e1, e2) {
+  class_check ("comparison", e1, e2)
+  return (scale_metric(e1, get_prefix(e2))@.Data != e2@.Data)
+})
+
 # Addition  ========================
- 
+
+#' @usage qty + qty, qty - qty
+#' @details
+#' \code{qty +- qty} allows the addition/subtraction of quantities that are the same type (e.g. all mass). Note that this operation also scales the new value using \code{\link{best_metric}}.
+#' @examples 
+#' qty(1000, "mg") + qty(999, "g") # 1 kg
+#' @name arithmetic 
+NULL
+
 setMethod("+", signature(e1 = "Quantity", e2 = "Quantity"), function(e1, e2) {
   class_check ("addition", e1, e2)
   return(best_metric(scale_metric(e1, get_prefix(e2)) + e2@.Data))
@@ -48,12 +79,45 @@ setMethod("-", signature(e1 = "Quantity", e2 = "Quantity"), function(e1, e2)  e1
 
 # Division =========================
 
+#' @usage qty / qty
+#' @details
+#' \code{qty / qty} divide quantities of the same type. Returns plain numeric (i.e. the units are divided out).
+#' @examples 
+#' qty(5, "mg") / qty(1, "g") # 0.005
+#' @name arithmetic 
+NULL
+
 setMethod("/", signature(e1 = "Quantity", e2 = "Quantity"), function(e1, e2) {
   if (class(e1) == class(e2))
     return (scale_metric(e1, get_prefix(e2))@.Data / e2@.Data)
   else
     operation_error ("division", e1, e2)
 })
+
+# don't allow number / quantity
+setMethod("/", signature(e1 = "numeric", e2 = "Quantity"), function(e1, e2) {
+  operation_error ("division", e1, e2)
+})
+
+# Multiplication =========================
+
+# don't allow default quantity * quantity
+setMethod("*", signature(e1 = "Quantity", e2 = "Quantity"), function(e1, e2) {
+  operation_error ("multiplication", e1, e2)
+})
+
+# Special operations ==============
+
+#' @usage amount / volume = molarity, amount / molarity = volume, molarity * volume = amount
+#' @details \code{amount / volume} divide an amount by a volume to create a molarity (concentration).
+#' @details \code{amount / molarity} divide an amount by a molarity to create a volume. 
+#' @details \code{molarity * volume} multiply a molarity by a volume (or the other way around) to create an amount.  
+#' @examples 
+#' qty(5, "nmol") / qty(50, "mL") # 100 nM
+#' qty(5, "nmol") / qty(100, "nM") # 50 mL
+#' qty(100, "nM") * qty(50, "mL") # 5 nmol
+#' @name arithmetic 
+NULL
 
 # amount / volume = molarity
 setMethod("/", signature(e1 = "Amount", e2 = "Volume"), function(e1, e2) {
@@ -70,6 +134,17 @@ setMethod("*", signature(e1 = "Volume", e2 = "Molarity"), function(e1, e2) {
   return (amount( base_metric(e1)@.Data * base_metric(e2)@.Data, "mol", scale = TRUE))
 })
 setMethod("*", signature(e1 = "Molarity", e2 = "Volume"), function(e1, e2) e2 * e1)
+
+#' @usage mass / MW = amount, mass / amount = MW, amount * MW = mass
+#' @details \code{mass / MW} divide a mass by a molecular weight to create an amount (mols).
+#' @details \code{mass / amount} divice a mass by an amount (mols) to create a molecular weight.
+#' @details \code{amount * MW} multiply an amount (mols) by a molecular weight to create a mass.
+#' @examples 
+#' qty(10, "g") / qty (50, "g/mol") # 200 mM 
+#' qty(10, "g") / qty(200, "mmol") # 50 g/mol
+#' qty(200, "mmol") * qty (50, "g/mol") # 10 g
+#' @name arithmetic 
+NULL
 
 # mass / MW = amount
 setMethod("/", signature(e1 = "Mass", e2 = "MolecularWeight"), function(e1, e2) {
--- tests/testthat/test-02-conversions.R
+++ tests/testthat/test-02-conversions.R
@@ -8,6 +8,8 @@ test_that("Testing that arithemtic calculations of units are working", {
   expect_true(qty(100, "mg") <= qty(0.1, "g"))
   expect_true(qty(100, "mg") > qty(10000, "µg"))
   expect_true(qty(10, "mg") >= qty(10000, "µg"))
+  expect_true(qty(10, "mg") == qty(10000, "µg"))
+  expect_false(qty(10, "mg") != qty(10000, "µg"))
   
   # addition
   expect_error(qty(2, "C") + qty(1, "g"), "addition is not implemented for these quantities")
@@ -17,23 +19,33 @@ test_that("Testing that arithemtic calculations of units are working", {
   
   # division
   expect_error(qty(2, "C") / qty(1, "g"), "division is not implemented for these quantities")
+  expect_error(1000 / qty(1, "g"), "division is not implemented for these quantities")
+  expect_equal(qty(1, "g") / 1000, qty(0.001, "g", scale_to_best_metric = FALSE))
   expect_equal(qty(1, "g") / qty(1, "mg"), 1000) # should give plain number
   expect_equal(qty(5, "mM") / qty(1, "M"), 0.005) # should give plain number
   
-  # amount / volume = concentration (all permutations)
+  # multiplication
+  expect_error(qty(1, "g") * qty(1, "g"), "multiplication is not implemented for these quantities")
+  expect_equal(qty(5, "g") * 100, qty(500, "g"))
+  expect_equal(100 * qty(5, "g"), qty(500, "g"))
+  
+  # amount / volume = concentration
   expect_equal(qty(5, "µmol") / qty(1, "ml"), qty(5, "mM"))
   expect_equal(qty(5, "pmol") / qty(10, "nL"), qty(500, "µM"))
+  # amount / molarity = volume
   expect_equal(qty(5, "nmol") / qty(10, "mM"), qty(500, "nL"))
+  # volume * molarity = amount
   expect_equal(qty(1, "ml") * qty(10, "mM"), qty(10, "µmol"))
   expect_equal(qty(10, "mM") * qty(1, "ml"), qty(10, "µmol"))
+  # chain: volume * molarity / volume = amount
   expect_equal(qty(1, "ml") * qty(10, "mM") / qty(1, "L"), qty(10, "µM"))
   expect_equal(qty(1, "ml") * qty(0.5, "mM") / qty(100, "mM"), qty(5, "µL"))
   
-  # mass / MW = amount (all permutations)
+  # mass / MW = amount
   expect_equal(qty(5, "mg") / qty(100, "g/mol"), qty(50, "µmol"))
+  # mass / amount = MW
   expect_equal(qty(2, "mg") / qty(1, "µmol"), qty(2000, "g/mol"))
+  # amount * MW = mass
   expect_equal(qty(10, "nmol") * qty(50, "g/mol"), qty(500, "ng"))
-  
-  # combining equations
   
 })
