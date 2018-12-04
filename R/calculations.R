054d991c25c877ee10ddae26ea8f0b2bf99f8b36
Sebastian Kopf <sebastian.kopf@colorado.edu>
2018-12-04 14:32:37 -0700

    implement basic calculations

---
+++ R/calculations.R
@@ -0,0 +1,67 @@
+#' Calculate the concentration of an ideal gas
+#' 
+#' Calculate the gas phase equivalent concentration of an ideal gas at a specific temperature (i.e. mol of gas / L of volume).
+#' @param pressure the pressure quantity of the gas
+#' @param temperature the temperature quantity of the gas
+#' @return molarity quantity
+#' @family calculations
+#' @export
+calculate_ideal_gas_molarity <- function(pressure, temperature) {
+  
+  require_quantity(rlang::enquo(pressure), is_pressure)
+  require_quantity(rlang::enquo(temperature), is_temperature)
+  
+  temperature.K <- as.numeric(base_metric(temperature))
+  pressure.bar <- as.numeric(base_metric(pressure))
+  R_ideal <- get_mediatools_constant("R_in_L_bar_per_K_mol") 
+  molarity.M <- pressure.bar / (R_ideal * temperature.K)
+  
+  return(molarity(molarity.M, "M"))
+}
+
+#' Calculate the amount of an ideal gas
+#' 
+#' Calculate the amount of an ideal gas at a specific pressure, temperature and volume
+#' @inheritParams calculate_ideal_gas_molarity
+#' @param volume the volume quantity of the gas
+#' @return amount quantity
+#' @family calculations
+#' @export
+calculate_ideal_gas_amount <- function(pressure, temperature, volume) {
+  molarity <- calculate_ideal_gas_molarity(pressure, temperature)
+  require_quantity(rlang::enquo(volume), is_volume)
+  return(molarity * volume)
+}
+
+#' Calculate solubility of a gas
+#' 
+#' Calculate Henry's law solubility constant for a gas at a specific temperature. Henry's law constants are from: Sander, R. Compilation of Henry's law constants (version 4.0) for water as solvent. Atmos Chem Phys 15, 4399–4981 (2015). https://www.atmos-chem-phys.net/15/4399/2015/
+#' 
+#' @param name of the gas
+#' @param temperature temperature quantity
+#' @family calculations
+#' @return solubility quantity
+calculate_solubility <- function(gas, temperature) {
+  
+  # consider storing more centrally for easy modification/extension?
+  constants <- 
+    tibble::tribble(
+      ~gas,   ~`dH/d(1/T)`, ~T0,     ~H0,       
+      # name, slope         K,       M/bar = 100 * mol/m3/Pa 
+      "CO2",  2400,         298.15,  3.3 * 10^-4 * 100
+    )
+  
+  # safety checks
+  if (missing(gas)) stop("gas is missing", call. = FALSE)
+  gas_constants <- dplyr::filter(constants, gas == !!gas)
+  if (nrow(gas_constants) == 0) 
+    stop("no constants stored for gas ", gas, call. = FALSE)
+  else if (nrow(gas_constants) > 1)
+    stop("more than one set of constants for gas ", gas, call. = FALSE)
+  require_quantity(rlang::enquo(temperature), is_temperature)
+  
+  # calculation
+  temperature.K <- as.numeric(base_metric(temperature))
+  KH <- with(gas_constants, H0 * exp(`dH/d(1/T)` * (1/temperature.K - 1/T0) ))
+  return(solubility(KH, "M/bar"))
+}
---
+++ tests/testthat/test-03-calculations.R
@@ -0,0 +1,32 @@
+context("Calculations")
+
+test_that("Testing that ideal gas calculations are working", {
+  
+  # ideal gas molarity
+  expect_error(calculate_ideal_gas_molarity(), "pressure is missing")
+  expect_error(calculate_ideal_gas_molarity(1), "not a pressure")
+  expect_error(calculate_ideal_gas_molarity(qty(1, "mbar")), "temperature is missing")
+  expect_error(calculate_ideal_gas_molarity(qty(1, "mbar"), 1), "not a temperature")
+  expect_equal(
+    calculate_ideal_gas_molarity(qty(100, "mbar"), qty(0, "C")) %>% base_metric() %>% as.numeric(),
+    0.1 / (273.15 * get_mediatools_constant("R_in_L_bar_per_K_mol"))
+  )
+  
+  # ideal gas amount
+  expect_error(calculate_ideal_gas_amount(qty(1, "mbar"), qty(0, "C")), "volume is missing")
+  expect_error(calculate_ideal_gas_amount(qty(1, "mbar"), qty(0, "C"), 1), "not a volume")
+  expect_equal(
+    calculate_ideal_gas_amount(qty(100, "mbar"), qty(0, "C"), qty(5, "mL")) %>% base_metric() %>% as.numeric(),
+    0.005 * 0.1 / (273.15 * get_mediatools_constant("R_in_L_bar_per_K_mol"))
+  )
+  
+  # solubility
+  expect_error(calculate_solubility(), "gas is missing")
+  expect_error(calculate_solubility("x"), "no constants")
+  expect_error(calculate_solubility("CO2"), "temperature is missing")
+  expect_true(is_solubility(s <- calculate_solubility("CO2", qty(25, "C"))))
+  expect_equal(get_qty_units(s), "mM/bar")
+  expect_equal(as.numeric(s), 33)
+  
+})
+  
