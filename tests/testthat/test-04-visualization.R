d707b66cee38a1d11ca437974a8f7b29b4a17485
Sebastian Kopf <sebkopf>
2020-10-16 23:57:49 -0600

    implement units axes

--- tests/testthat/test-04-visualization.R
+++ tests/testthat/test-04-visualization.R
@@ -1,13 +1,7 @@
 context("Visualization")
 
-test_that("test that quantity labeller works", {
+test_that("test that ggplot extension works", {
   
-  expect_error(make_qty_text_labeller(), "requires a quantity")
-  expect_error(make_qty_text_labeller(1), "requires a quantity")
-  q <- qty(c(0.1, 1, 1000), "g")
-  expect_is(l <- make_qty_text_labeller(q), "labeller")
-  expect_equal(l(q), c("100 mg", "1 g", "1 kg"))
-  expect_is(l <- make_qty_text_labeller(q, unit = "g"), "labeller")
-  expect_equal(l(q), c("0.1 g", "1 g", "1000 g"))
+  # TODO: implement tests in addition to the examples
   
 })
