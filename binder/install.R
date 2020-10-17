35ab02d74d696a366a8cca18dc6450354d1cc341
Sebastian Kopf <sebastian.kopf@colorado.edu>
2020-10-17 00:06:45 -0600

    Update install.R

--- binder/install.R
+++ binder/install.R
@@ -1,6 +1,6 @@
 # Package itself with all dependencies (including suggested packages)
 install.packages("devtools")
-devtools::install_github("KopfLab/mediatools", ref = "binder", dependencies = TRUE)
+devtools::install_github("KopfLab/microbialkitchen", ref = "binder", dependencies = TRUE)
 
 # Packages for knitting
 install.packages(c("rmarkdown", "caTools", "bitops")) 
